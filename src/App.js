9e5b52b0ef3c08a0127b66ab4b80189e5312036d
PovilasGe <povilasgedminas@gmail.com>
2022-12-30 21:41:17 +0200

    add route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import logo from './logo.svg';
+import {Route, Routes } from 'react-router-dom';
+import React from 'react';
+import {Container} from '@mui/system';
+import { Grid } from '@mui/material';
+import Profile from "./components/Profile/Profile"
+import Header from "./components/Header/Header"
+import Footer from "./components/Footer/Footer"
+import Portfolio from "./pages/Portfolio/Portfolio"
+import Resume from "./pages/Resume/Resume"
+
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+<Container>
+  <Grid container>
+    <Grid item xs={12} sm={12} md={4} lg={3} style={{backgroundColor: "blue"}}>
+      <Profile />
+    </Grid>
+    <Grid item xs style={{backgroundColor: "red"}}>
+      <Header />
+      <Routes>
+        <Route path='/portfolio' element={<Portfolio />} />
+        <Route path='/' element={<Resume />} />
+      </Routes>
+      <Footer />
+    </Grid>
+  </Grid>
+</Container>
   );
 }
 
---
+++ src/pages/Resume/Resume.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Resume = () => {
+  return (
+    <div>
+        Portfolio page
+    </div>
+  )
+}
+
+export default Resume
