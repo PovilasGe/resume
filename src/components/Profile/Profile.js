64e71a8587defcca89567742b39ca21a970b2801
PovilasGe <povilasgedminas@gmail.com>
2023-01-02 12:12:20 +0200

    add styles for profile and timeline

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,5 +1,6 @@
 import { Typography } from '@mui/material'
 import React from 'react'
+import CustomTimeline from '../Timeline/Timeline'
 
 import './Profile.css'
 
@@ -7,8 +8,8 @@ const Profile = () => {
   return (
     <div className='profile container_shadow'>
       <div className='profile_name'>
-        <Typography className='name'>Sam</Typography>
-        <Typography className='title'>Title</Typography>
+        <Typography className='name'>Povilas Gedminas</Typography>
+        <Typography className='title'>Front end developer</Typography>
 
       </div>
       <figure className='profile_image'>
@@ -16,7 +17,7 @@ const Profile = () => {
       </figure>
 
       <div className='profile_information'>
-        Instert Timeline
+        <CustomTimeline />
         <br/>
         <button>my button</button>
       </div>
