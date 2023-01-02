c49353ec5c191f48af0eb233217431454034e91a
PovilasGe <povilasgedminas@gmail.com>
2023-01-02 10:15:47 +0200

    add profile style

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,8 +1,25 @@
+import { Typography } from '@mui/material'
 import React from 'react'
+
+import './Profile.css'
 
 const Profile = () => {
   return (
-    <div>
+    <div className='profile container_shadow'>
+      <div className='profile_name'>
+        <Typography className='name'>Sam</Typography>
+        <Typography className='title'>Title</Typography>
+
+      </div>
+      <figure className='profile_image'>
+        <img src={require('../../assets/images/displayImage.JPG')} alt=''/>
+      </figure>
+
+      <div className='profile_information'>
+        Instert Timeline
+        <br/>
+        <button>my button</button>
+      </div>
     </div>
   )
 }
