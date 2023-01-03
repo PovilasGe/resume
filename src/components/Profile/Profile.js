49b4da2e891fd7769135f5ee1afaf319f7ec877f
PovilasGe <povilasgedminas@gmail.com>
2023-01-03 15:30:01 +0200

    add button

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -6,16 +6,25 @@ import resumeData from '../../utils/resumeData'
 import './Profile.css'
 import { TimelineItem } from '@mui/lab';
 import { TimelineContent } from '@mui/lab';
+import CustomButton from '../Button/Button';
+import GetAppIcon from '@mui/icons-material/GetApp';
 
 const CustomTimelineItem =({title, text, link}) => (
   <TimelineItem >
     <CustomTimelineSeparator />
     <TimelineContent className='timeline_content'>
-      {link ? (<Typography className='timelineItem_text'><span>{title}:</span> <a href={link} target='_blank'>{text}</a> </Typography>) : (
-        <Typography className='timelineItem_text'><span>{title}:</span>{text}</Typography>
-      )}
-      
-    </TimelineContent>
+  {link ? (
+    <Typography className='timelineItem_text'>
+      <span>{title}:</span> 
+      <a href={link} target='_blank' rel='noreferrer'>{text}</a> 
+    </Typography>
+  ) : (
+    <Typography className='timelineItem_text'>
+      <span>{title}:</span>{text}
+    </Typography>
+  )}
+</TimelineContent>
+
   </TimelineItem>
 )
 
@@ -38,11 +47,12 @@ const Profile = () => {
         <CustomTimelineItem title='Email' text={resumeData.email}/>
 
         {Object.keys(resumeData.socials).map((key) => (
-         <CustomTimelineItem title={key} text={resumeData.socials[key].text} link={resumeData.socials[key].link}/> 
+         <CustomTimelineItem key={key} title={key} text={resumeData.socials[key].text} link={resumeData.socials[key].link}/> 
         ))}
         </CustomTimeline>
-        <br/>
-        <button>my button</button>
+        <div className='button_container'>
+         <CustomButton text='Download CV' icon={<GetAppIcon/>}/>
+        </div>
       </div>
     </div>
   )
